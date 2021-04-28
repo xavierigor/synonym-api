10bc5373c5479682baa6aaf20ad7dff1cc8f6fbd
Igor Xavier <igoorlimaa@gmail.com>
2021-04-28 20:08:15 -0300

    Project setup

---
+++ config/settings.py
@@ -0,0 +1,84 @@
+import environ
+
+env = environ.Env()
+root_path = environ.Path(__file__) - 2
+env.read_env(env_file=root_path(".env"))
+
+
+# -----------------------------------------------------------------------------
+# Basic Config
+# -----------------------------------------------------------------------------
+ENV = env("DJANGO_ENV", default="prod")
+assert ENV in ["dev", "test", "prod", "qa"]
+
+DEBUG = env.bool("DEBUG", default=False)
+BASE_DIR = root_path()
+ROOT_URLCONF = "config.urls"
+WSGI_APPLICATION = "config.wsgi.application"
+
+
+# -----------------------------------------------------------------------------
+# Time & Language
+# -----------------------------------------------------------------------------
+LANGUAGE_CODE = env("LANGUAGE_CODE", default="en-us")
+TIME_ZONE = env("TIMEZONE", default="UTC")
+USE_I18N = env("USE_I18N", default=True)
+USE_L10N = env("USE_L10N", default=True)
+USE_TZ = env("USE_TZ", default=True)
+
+
+# -----------------------------------------------------------------------------
+# Security and Users
+# -----------------------------------------------------------------------------
+SECRET_KEY = env("SECRET_KEY")
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
+
+
+# -----------------------------------------------------------------------------
+# Databases
+# -----------------------------------------------------------------------------
+DJANGO_DATABASE_URL = env.db("DATABASE_URL")
+DATABASES = {"default": DJANGO_DATABASE_URL}
+
+
+# -----------------------------------------------------------------------------
+# Applications configuration
+# -----------------------------------------------------------------------------
+INSTALLED_APPS = [
+    # Third party
+    "rest_framework",
+    "corsheaders",
+
+    # Local
+    # ...
+]
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# -----------------------------------------------------------------------------
+# DRF configuration
+# -----------------------------------------------------------------------------
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'UNAUTHENTICATED_USER': None,
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter'
+    ],
+}
